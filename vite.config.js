d721561af5b16ee3bf98a427b14195c4362e79a8
Yogadharshni <yogdharshni06@gmail.com>
2023-08-25 15:59:29 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: [
-        /^node:.*/,
-      ]
-    }
-  }
+  plugins: [react()],
 })
+
